7201ee375fd26a9ff2a9c3be9ccddb1ec2cd9e94
ErhanArda <erhanarda7@gmail.com>
2023-11-04 14:03:30 +0300

    update: filteredCharacters

--- Week4/nextjs-example/rick-and-morty-project/src/pages/characters/index.js
+++ Week4/nextjs-example/rick-and-morty-project/src/pages/characters/index.js
@@ -1,19 +1,8 @@
 import { useEffect, useState } from 'react';
 import { fetchCharacters } from '../api';
-import {
-  Autocomplete,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  IconButton,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Stack, TextField } from '@mui/material';
 import { useRouter } from 'next/router';
+import InfoCard from '@/components/InfoCard';
 
 const Characters = () => {
   const router = useRouter();
@@ -68,15 +57,6 @@ const Characters = () => {
   const isFavorite = (id) => favorites.includes(id);
   return (
     <Stack spacing={2}>
-      {/* <Autocomplete
-        id="combo-box"
-        options={characters}
-        getOptionLabel={(character) => character.name}
-        onChange={handleCharacterSelect}
-        sx={{ width: 300 }}
-        size="small"
-        renderInput={(params) => <TextField {...params} label="Character" />}
-      /> */}
       <TextField
         label="Search"
         variant="outlined"
@@ -93,39 +73,13 @@ const Characters = () => {
         flexWrap="wrap"
       >
         {filteredCharacters.map((character) => (
-          <Card key={character.id} sx={{ width: '150px' }}>
-            <CardMedia
-              component="img"
-              alt="character"
-              height="140"
-              image={character.image}
-            />
-            <CardContent>
-              <Typography variant="h5" component="div">
-                {character.name}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {character.gender} {character.status}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <IconButton
-                aria-label="add to favorites"
-                onClick={() => handleFavoriteToggle(character.id)}
-              >
-                <FavoriteIcon
-                  color={isFavorite(character.id) ? 'error' : 'disabled'}
-                />
-              </IconButton>
-              <Button
-                size="small"
-                variant="text"
-                onClick={() => handleClick(character.id)}
-              >
-                Learn More
-              </Button>
-            </CardActions>
-          </Card>
+          <InfoCard
+            key={character.id}
+            character={character}
+            handleFavoriteToggle={handleFavoriteToggle}
+            isFavorite={isFavorite}
+            handleClick={handleClick}
+          />
         ))}
       </Stack>
     </Stack>
