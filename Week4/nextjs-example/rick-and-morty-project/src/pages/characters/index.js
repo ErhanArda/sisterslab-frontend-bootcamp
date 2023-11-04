4980845c124b29b434c391e28a05517dbcd625cc
ErhanArda <erhanarda7@gmail.com>
2023-11-04 13:33:11 +0300

    add: searchbar

--- Week4/nextjs-example/rick-and-morty-project/src/pages/characters/index.js
+++ Week4/nextjs-example/rick-and-morty-project/src/pages/characters/index.js
@@ -17,9 +17,11 @@ import { useRouter } from 'next/router';
 
 const Characters = () => {
   const router = useRouter();
-
   const [characters, setCharacters] = useState([]);
   const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const [favorites, setFavorites] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
   const getCharacters = async () => {
     const characters = await fetchCharacters();
     setCharacters(characters);
@@ -27,23 +29,46 @@ const Characters = () => {
 
   useEffect(() => {
     getCharacters();
+
+    const storedFavorites = localStorage.getItem('favorites');
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites));
+    }
   }, []);
 
-  const handleCharacterSelect = (e, value) => {
-    setSelectedCharacter(value);
-  };
+  // const handleCharacterSelect = (e, value) => {
+  //   setSelectedCharacter(value);
+  // };
 
   const handleClick = (id) => {
     router.push(`/characters/${id}`);
   };
 
-  const filteredCharacters = selectedCharacter
-    ? characters.filter((character) => character.id === selectedCharacter.id)
-    : characters;
+  // const filteredCharacters = selectedCharacter
+  //   ? characters.filter((character) => character.id === selectedCharacter.id)
+  //   : characters;
 
+  const filteredCharacters = characters.filter((character) => {
+    return (
+      character.name.toLowerCase().includes(searchText.toLowerCase()) ||
+      character.status.toLowerCase().includes(searchText.toLowerCase()) ||
+      character.gender.toLowerCase().includes(searchText.toLowerCase())
+    );
+  });
+
+  const handleFavoriteToggle = (id) => {
+    const newFavorites = favorites.includes(id)
+      ? favorites.filter((favoriteId) => favoriteId !== id)
+      : [...favorites, id];
+
+    setFavorites(newFavorites);
+    localStorage.setItem('favorites', JSON.stringify(newFavorites));
+  };
+
+  const isFavorite = (id) => favorites.includes(id);
   return (
     <Stack spacing={2}>
-      <Autocomplete
+      {/* <Autocomplete
         id="combo-box"
         options={characters}
         getOptionLabel={(character) => character.name}
@@ -51,6 +76,15 @@ const Characters = () => {
         sx={{ width: 300 }}
         size="small"
         renderInput={(params) => <TextField {...params} label="Character" />}
+      /> */}
+      <TextField
+        label="Search"
+        variant="outlined"
+        size="small"
+        fullWidth
+        placeholder="Search by name, status, gender"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
       />
       <Stack
         spacing={{ xs: 1, sm: 2 }}
@@ -75,8 +109,13 @@ const Characters = () => {
               </Typography>
             </CardContent>
             <CardActions>
-              <IconButton aria-label="add to favorites">
-                <FavoriteIcon color="error" />
+              <IconButton
+                aria-label="add to favorites"
+                onClick={() => handleFavoriteToggle(character.id)}
+              >
+                <FavoriteIcon
+                  color={isFavorite(character.id) ? 'error' : 'disabled'}
+                />
               </IconButton>
               <Button
                 size="small"
