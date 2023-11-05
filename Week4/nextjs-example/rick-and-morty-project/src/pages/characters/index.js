4071c9343edb6cd5bcbd36785d40a6240f865c96
ErhanArda <erhanarda7@gmail.com>
2023-11-05 12:56:23 +0300

    add: search button

--- Week4/nextjs-example/rick-and-morty-project/src/pages/characters/index.js
+++ Week4/nextjs-example/rick-and-morty-project/src/pages/characters/index.js
@@ -1,23 +1,49 @@
 import { useEffect, useState } from 'react';
-import { fetchCharacters } from '../api';
-import { Stack, TextField } from '@mui/material';
+import { fetchCharacterWithSearch, fetchCharacters } from '../api';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { useRouter } from 'next/router';
 import InfoCard from '@/components/InfoCard';
 
 const Characters = () => {
   const router = useRouter();
   const [characters, setCharacters] = useState([]);
-  const [selectedCharacter, setSelectedCharacter] = useState(null);
   const [favorites, setFavorites] = useState([]);
   const [searchText, setSearchText] = useState('');
+  const [searchBy, setSearchBy] = useState('name');
+  const [errorFetch, setErrorFetch] = useState(null);
 
-  const getCharacters = async () => {
-    const characters = await fetchCharacters();
-    setCharacters(characters);
+  const fetchCharacterData = () => {
+    if (searchText) {
+      fetchCharacterWithSearch(searchText, searchBy)
+        .then((charactersList) => {
+          setCharacters(charactersList);
+          setErrorFetch(null);
+        })
+        .catch((error) => {
+          setErrorFetch(error.message || 'Bir hata oluştu');
+        });
+    } else {
+      fetchCharacters()
+        .then((charactersList) => {
+          setCharacters(charactersList);
+          setErrorFetch(null);
+        })
+        .catch((error) => {
+          setErrorFetch(error.message || 'Bir hata oluştu');
+        });
+    }
   };
-
   useEffect(() => {
-    getCharacters();
+    fetchCharacterData();
 
     const storedFavorites = localStorage.getItem('favorites');
     if (storedFavorites) {
@@ -25,25 +51,9 @@ const Characters = () => {
     }
   }, []);
 
-  // const handleCharacterSelect = (e, value) => {
-  //   setSelectedCharacter(value);
-  // };
-
   const handleClick = (id) => {
     router.push(`/characters/${id}`);
   };
-
-  // const filteredCharacters = selectedCharacter
-  //   ? characters.filter((character) => character.id === selectedCharacter.id)
-  //   : characters;
-
-  const filteredCharacters = characters.filter((character) => {
-    return (
-      character.name.toLowerCase().includes(searchText.toLowerCase()) ||
-      character.status.toLowerCase().includes(searchText.toLowerCase()) ||
-      character.gender.toLowerCase().includes(searchText.toLowerCase())
-    );
-  });
 
   const handleFavoriteToggle = (id) => {
     const newFavorites = favorites.includes(id)
@@ -57,30 +67,57 @@ const Characters = () => {
   const isFavorite = (id) => favorites.includes(id);
   return (
     <Stack spacing={2}>
-      <TextField
-        label="Search"
-        variant="outlined"
-        size="small"
-        fullWidth
-        placeholder="Search by name, status, gender"
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-      />
+      <Stack direction="row" spacing={2}>
+        <FormControl size="small" sx={{ width: '150px' }}>
+          <InputLabel id="demo-simple-select-label">Search By</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={searchBy}
+            label="Age"
+            onChange={(e) => setSearchBy(e.target.value)}
+          >
+            <MenuItem value="name">Name</MenuItem>
+            <MenuItem value="status">Status</MenuItem>
+            <MenuItem value="species">Species</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          label="Search"
+          variant="outlined"
+          size="small"
+          fullWidth
+          placeholder="Enter search text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={fetchCharacterData}
+        >
+          Search
+        </Button>
+      </Stack>
       <Stack
         spacing={{ xs: 1, sm: 2 }}
         direction="row"
         useFlexGap
         flexWrap="wrap"
       >
-        {filteredCharacters.map((character) => (
-          <InfoCard
-            key={character.id}
-            character={character}
-            handleFavoriteToggle={handleFavoriteToggle}
-            isFavorite={isFavorite}
-            handleClick={handleClick}
-          />
-        ))}
+        {errorFetch ? (
+          <Typography color="error">{errorFetch}</Typography>
+        ) : (
+          characters?.map((character) => (
+            <InfoCard
+              key={character.id}
+              character={character}
+              handleFavoriteToggle={handleFavoriteToggle}
+              isFavorite={isFavorite}
+              handleClick={handleClick}
+            />
+          ))
+        )}
       </Stack>
     </Stack>
   );
