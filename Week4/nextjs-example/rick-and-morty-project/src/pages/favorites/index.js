8218176462052ef646391c27abbef2a2af9e976b
ErhanArda <erhanarda7@gmail.com>
2023-11-05 14:09:58 +0300

    add: useMemo& useCallback

--- Week4/nextjs-example/rick-and-morty-project/src/pages/favorites/index.js
+++ Week4/nextjs-example/rick-and-morty-project/src/pages/favorites/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { fetchCharacters } from '../api';
 import { Stack, Typography } from '@mui/material';
 import InfoCard from '@/components/InfoCard';
@@ -6,7 +6,13 @@ import InfoCard from '@/components/InfoCard';
 const Favorites = () => {
   const [characters, setCharacters] = useState([]);
   const [favorites, setFavorites] = useState([]);
-
+  useEffect(() => {
+    getCharacters();
+    const storedFavorites = localStorage.getItem('favorites');
+    if (storedFavorites) {
+      setFavorites(JSON.parse(storedFavorites));
+    }
+  }, []);
   const getCharacters = async () => {
     const characters = await fetchCharacters();
     setCharacters(characters);
@@ -16,27 +22,42 @@ const Favorites = () => {
     favorites.includes(character.id)
   );
 
-  const handleFavoriteToggle = (id) => {
-    const newFavorites = favorites.includes(id)
-      ? favorites.filter((favoriteId) => favoriteId !== id)
-      : [...favorites, id];
+  // const handleFavoriteToggle = (id) => {
+  //   const newFavorites = favorites.includes(id)
+  //     ? favorites.filter((favoriteId) => favoriteId !== id)
+  //     : [...favorites, id];
 
-    setFavorites(newFavorites);
-    localStorage.setItem('favorites', JSON.stringify(newFavorites));
-  };
+  //   setFavorites(newFavorites);
+  //   localStorage.setItem('favorites', JSON.stringify(newFavorites));
+  // };
 
-  useEffect(() => {
-    getCharacters();
-    const storedFavorites = localStorage.getItem('favorites');
-    if (storedFavorites) {
-      setFavorites(JSON.parse(storedFavorites));
-    }
-  }, []);
+  const handleFavoriteToggle = useCallback(
+    (id) => {
+      const newFavorites = favorites.includes(id)
+        ? favorites.filter((favoriteId) => favoriteId !== id)
+        : [...favorites, id];
+
+      setFavorites(newFavorites);
+      localStorage.setItem('favorites', JSON.stringify(newFavorites));
+    },
+    [favorites]
+  );
+
+  // const favoriteCount = characters.filter((character) =>
+  //   favorites.includes(character.id)
+  // ).length;
+
   const isFavorite = (id) => favorites.includes(id);
+
+  const favoriteCount = useMemo(() => {
+    return characters.filter((character) => favorites.includes(character.id))
+      .length;
+  }, [favorites, characters]);
 
   return (
     <Stack spacing={2}>
       <Typography variant="h1">Your Favorite Characters</Typography>
+      <Typography>favori sayim: {favoriteCount}</Typography>
       <Stack
         spacing={{ xs: 1, sm: 2 }}
         direction="row"
