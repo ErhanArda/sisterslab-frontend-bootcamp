0c9bf2c621dc27027e6baad05dffb20f22293e4a
ErhanArda <erhanarda7@gmail.com>
2023-11-11 15:08:07 +0300

    Add: dependencies and update home page.

--- Week6/book-shelf/src/pages/index.js
+++ Week6/book-shelf/src/pages/index.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const index = () => {
-  return <div>index</div>;
+const Home = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/books')
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((error) => console.error('error :>> ', error));
+  }, []);
+
+  return (
+    <Grid container spacing={2}>
+      {books &&
+        books.map((book) => (
+          <Grid item key={book.id} xs={12} sm={6} md={4}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={book.imageUrl ?? 'https://picsum.photos/200/300'}
+                  alt={book.title}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {book.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {book.description.substring(0, 100)}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+    </Grid>
+  );
 };
 
-export default index;
+export default Home;
