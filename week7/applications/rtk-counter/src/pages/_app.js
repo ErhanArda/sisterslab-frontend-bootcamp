845b44a65d25f21a53e0716f258a1962dd94939a
ErhanArda <erhanarda7@gmail.com>
2023-11-18 13:53:24 +0300

    Add Redux store configuration and counterSlice reducer

--- week7/applications/rtk-counter/src/pages/_app.js
+++ week7/applications/rtk-counter/src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { store } from '@/store';
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
