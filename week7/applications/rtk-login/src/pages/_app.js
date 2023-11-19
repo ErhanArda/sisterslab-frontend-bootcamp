f030fd3a3a3edaf254aa3fe9ec85301556ff841d
ErhanArda <erhanarda7@gmail.com>
2023-11-19 12:07:48 +0300

    Add Redux store configuration and Provider component

--- week7/applications/rtk-login/src/pages/_app.js
+++ week7/applications/rtk-login/src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { Provider } from 'react-redux';
+import { store } from '@/store';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
