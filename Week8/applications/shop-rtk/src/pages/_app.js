ae5a635f604fea049c1e6d50be505c9cae1a3643
ErhanArda <erhanarda7@gmail.com>
2023-11-25 11:32:33 +0300

    Add Redux store configuration

--- Week8/applications/shop-rtk/src/pages/_app.js
+++ Week8/applications/shop-rtk/src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
